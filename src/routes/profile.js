8dc2a6ca3b451e39681baa6c4ec40ac946f3c750
lekkss <leksguy97@gmail.com>
2023-12-28 21:49:09 +0000

    added password change

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,13 @@
 import express from "express";
-import { getProfile } from "../controllers/profile.js";
+import { changePassword, getProfile } from "../controllers/profile.js";
+import validateRequest from "../middlewares/validate.js";
+import { passwordSchema } from "../validations/userValidation.js";
 
 const router = express.Router();
 router.get("/", getProfile);
+router.post(
+  "/update/password",
+  validateRequest(passwordSchema),
+  changePassword
+);
 export default router;
